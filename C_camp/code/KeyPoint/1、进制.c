8b93a861ec21b4ddace8d73f2fcac92ac2839624
aqlzh <lzh_create@qq.com>
2021-10-12 19:07:06 +0800

    feat:(21/10/12) 代码中进制的表示

---
+++ C_camp/code/KeyPoint/1、进制.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main01() {
+
+	 
+	int i = 123 ;  // 十进制
+	int j = 0x7B;  // 十六进制
+	int k = 0173; //八进制
+
+	printf("%d\n", i);  // %d 以十进制输出 
+	printf("%d\n", j); 
+	printf("%d\n", k); 
+
+}
